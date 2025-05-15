204c63bab9bc3a89119533c79ec2fb0e2d7607ce
maros22 <61597602+maros22@users.noreply.github.com>
2025-05-15 17:08:51 +0200

    Update modbus_discrete_inputs.h

--- components/modbus_discrete_inputs/modbus_discrete_inputs.h
+++ components/modbus_discrete_inputs/modbus_discrete_inputs.h
@@ -1,1 +1,23 @@
-fff
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/modbus_controller/modbus_controller.h"
+
+namespace esphome {
+namespace modbus_discrete_inputs {
+
+class ModbusDiscreteInputReader : public Component {
+ public:
+  ModbusDiscreteInputReader(modbus_controller::ModbusController *modbus, uint8_t address)
+      : modbus_(modbus), address_(address) {}
+
+  void setup() override;
+  void loop() override;
+
+ protected:
+  modbus_controller::ModbusController *modbus_;
+  uint8_t address_;
+};
+
+}  // namespace modbus_discrete_inputs
+}  // namespace esphome
